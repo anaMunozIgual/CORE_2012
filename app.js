bad113e3e5c26255cce44e78de548f89e766b34d
Ana <ami@ubuntu.(none)>
2013-05-11 10:40:10 +0200

    tema 3 despliegue en heroku

--- app.js
+++ app.js
@@ -1,48 +1,55 @@
+
 /**
  * Module dependencies.
  */
+
 var express = require('express')
- , routes = require('./routes')
- , user = require('./routes/user')
- , http = require('http')
- , path = require('path')
- , partials = require('express-partials')
- , postController = require('./routes/post_controller.js');
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path')
+  , partials = require('express-partials')
+  , postController = require('./routes/post_controller.js');
 
 var app = express();
+
 app.use(partials());
+
 app.configure(function(){
- app.set('port', process.env.PORT || 3000);
- app.set('views', __dirname + '/views');
- app.set('view engine', 'ejs');
- app.use(express.favicon());
- app.use(express.logger('dev'));
- app.use(express.bodyParser());
- app.use(express.methodOverride());
- app.use(express.cookieParser('your secret here'));
- app.use(express.session());
- app.use(app.router);
- app.use(express.static(path.join(__dirname, 'public')));
-});
-app.configure('development', function(){
- app.use(express.errorHandler());
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'ejs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-//Helper escapeText para evitar la inyección de código sin
-//destruir la maquetación del usuario.
-app.locals.escapeText = function(text) {
-	return String(text)
-			.replace(/&(?!\w+;)/g, '&amp;')
-			.replace(/</g, '&lt;')
-			.replace(/>/g, '&gt;')
-			.replace(/"/g, '&quot;')
-			.replace(/\n/g, '<br>');
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+// Helper estatico:
+app.locals.escapeText =  function(text) {
+   return String(text)
+          .replace(/&(?!\w+;)/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/\n/g, '<br>');
 };
 
-//Routes
+// -- Routes
+
 app.get('/', routes.index);
 app.get('/users', user.list);
-//---------------------------
+
+//---------------------
+
 app.get('/posts.:format?', postController.index);
 app.get('/posts/new', postController.new);
 app.get('/posts/:postid([0-9]+).:format?', postController.show);
@@ -52,6 +59,8 @@ app.put('/posts/:postid([0-9]+)', postController.update);
 app.delete('/posts/:postid([0-9]+)', postController.destroy);
 app.get('/posts/search', postController.search);
 
+//---------------------
+
 http.createServer(app).listen(app.get('port'), function(){
- console.log("Express server listening on port " + app.get('port'));
+  console.log("Express server listening on port " + app.get('port'));
 });
